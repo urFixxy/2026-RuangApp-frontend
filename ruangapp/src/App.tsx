bd0c0779205a4af178a03ca26178beea06667f46
fitda <fitdafan@gmail.com>
2026-02-16 09:13:59 +0700

    setup: Initialize React with Typescript + Tailwind

---
+++ ruangapp/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Vite + Tailwind Works 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
+
